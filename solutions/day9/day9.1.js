616d9a3e458f5250697ba6bb6239e4f0cf63065e
pfg <omegariisthebest@gmail.com>
2019-12-08 22:16:19 -0800

    New intcode parser

--- solutions/day9/day9.1.js
+++ solutions/day9/day9.1.js
@@ -1,270 +1,117 @@
-// base intcode computer to copy from
-// after completing both parts of a level using this, go back and update this with changes in better code quality
-
-let gid = 0;
-class M {
-	constructor() {
-		this.id = gid++;
+async function intcode(
+	input,
+	getInput /*: () => Promise<bigint>*/,
+	writeOutput /*: (v: bigint) => void */,
+) {
+	let memory = input.split(",").map(w => BigInt(+w));
+	for (let i = 0; i < 10000; i++) {
+		memory.push(0n);
 	}
-	reset() {
-		this.vimmediate = undefined;
-		this.waiting = undefined;
-		this.onsent = undefined;
-	}
-	emit(v) {
-		return new Promise((resolve, reject) => {
-			if (this.waiting) {
-				this.waiting(v);
-				this.reset();
-				resolve();
-				return;
-			}
-			this.vimmediate = v;
-			this.onsent = () => {
-				this.reset();
-				resolve();
-			};
-		});
-	}
-	next() {
-		return new Promise((resolve, reject) => {
-			if (this.vimmediate !== undefined) {
-				resolve(this.vimmediate);
-				this.onsent();
-				return;
-			}
-			this.waiting = v => {
-				resolve(v);
-				return;
-			};
-		});
-	}
-}
+	let instructionPointer = 0n;
 
-function runComputer(instrv) {
-	let instream = new M();
-	let outstream = new M();
-	let donestream = new M();
+	let relativeBase = 0n;
 
-	let over = { v: false };
+	let parameterModes = [];
+	let parameterIndex = 0n;
 
-	setTimeout(async (reslv, rejec) => {
-		let inv = instrv.split`,`;
-		let output = [];
+	let exit = false;
 
-		class IntcodeComputer {
-			constructor(inv) {
-				this.memory = inv.map(v => BigInt(+v));
-				for (let i = 0; i < 10000; i++) {
-					this.memory.push(0n);
-				}
-				this.go = true;
-				this.index = 0;
-				this.relativebase = 0n;
-			}
-			stop() {
-				this.go = false;
-			}
-			get i() {
-				return this.index;
-			}
-			set i(nv) {
-				let prevV = this.index;
-				this.index = nv;
-			}
-			get n() {
-				// *(index++)
-				return this.getAt(this.i++);
-			}
-			get p() {
-				this.pmindex++;
-				let mode = this.parameterMode[this.pmindex];
-				if (mode === 0) {
-					return this.getAt(this.n);
-				}
-				if (mode === 1) {
-					return this.n;
-				}
-				if (mode === 2) {
-					let fasd = this.getAt(this.i);
-					console.log(
-						"reading relativemode",
-						fasd,
-						this.relativebase,
-						this.relativebase + fasd,
-					);
-					return this.getAt(this.relativebase + this.n);
-				}
-				throw new Error(
-					"invalid parameter mode " + this.parameterMode[this.pmindex],
-				);
-			}
-			get w() {
-				this.pmindex++;
-				let mode = this.parameterMode[this.pmindex];
-				console.log("w in relative mode");
-				if (mode === 2) {
-					let fasd = this.getAt(this.i);
-					console.log(
-						"reading relativemode w",
-						fasd,
-						this.relativebase,
-						this.relativebase + fasd,
-					);
-					return this.relativebase + this.n;
-				}
-				return this.n;
-			}
-			get m() {
-				return this.memory;
-			}
-			boundsCheck(index) {
-				if (index > this.m.length - 1 || index < 0) {
-					throw new Error(
-						"out of bounds (" + index + " / " + this.m.length + ")",
-					);
-				}
-			}
-			getAt(index) {
-				this.boundsCheck(index);
-				return this.memory[index];
-			}
-			setAt(index, value) {
-				this.boundsCheck(index);
-				let prevV = this.memory[index];
-				this.memory[index] = value;
-				return prevV;
-			}
-			adjustBase(v) {
-				// console.log("adjustbase", v, this.relativebase);
-				this.relativebase += v;
-				// console.log("adjustbase", v, this.relativebase);
-			}
+	let getPointer_ = () => {
+		let parameterMode = parameterModes[parameterIndex];
+		if (parameterMode === 0) {
+			return memory[instructionPointer + parameterIndex];
 		}
-		let mkv = das =>
-			new Proxy(new IntcodeComputer(das), {
-				get: (obj, prop) =>
-					(+prop).toString() === prop ? obj.getAt(+prop) : obj[prop],
-				set: (obj, prop, value) =>
-					(+prop).toString() === prop
-						? obj.setAt(+prop, value)
-						: ((obj[prop] = value), true),
-			});
-		let v = mkv(inv);
-		// adding an intcode:
-		// v.p: read as parameter
-		// v.w: read as output
-		let intcodes = {
-			"1": () => {
-				let [a, b, out] = [v.p, v.p, v.w];
-				v[out] = a + b;
-			},
-			"2": () => {
-				let [a, b, out] = [v.p, v.p, v.w];
-				v[out] = a * b;
-			},
-			"3": async () => {
-				let [addr] = [v.w];
-				v[addr] = await instream.next();
-				console.log("wrote to", addr, v[addr]);
-			},
-			"4": async () => {
-				// console.log(v);
-				let [a] = [v.p];
-				await outstream.emit(a);
-			},
-			"5": () => {
-				let [f, iftrue] = [v.p, v.p];
-				if (f !== 0) {
-					v.i = iftrue;
-				}
-			},
-			"6": () => {
-				let [f, iffalse] = [v.p, v.p];
-				if (f === 0) {
-					v.i = iffalse;
-				}
-			},
-			"7": () => {
-				let [f, s, out] = [v.p, v.p, v.w];
-				if (f < s) v[out] = 1;
-				else v[out] = 0;
-			},
-			"8": () => {
-				let [f, s, out] = [v.p, v.p, v.w];
-				if (f === s) v[out] = 1;
-				else v[out] = 0;
-			},
-			"9": () => {
-				let [adj] = [v.p];
-				console.log("adjusting base by", adj);
-				v.adjustBase(adj);
-				console.log("adjusting base by", v.relativebase);
-			},
-			"99": () => {
-				return v.stop();
-			},
-			"404": () => {
-				console.log("uh oh! invalid instruction", v[v.i - 1]);
-				return v.stop();
-			},
-		};
+		if (parameterMode === 1) {
+			return instructionPointer + parameterIndex;
+		}
+		if (parameterMode === 2) {
+			return relativeBase + memory[instructionPointer + parameterIndex];
+		}
+	};
+	let getPointer = () => {
+		let res = getPointer_();
+		parameterIndex++;
+		return res;
+	};
+	let getValue = () => memory[getPointer()];
+	let p = getPointer;
+	let v = getValue;
 
-		while (v.go) {
-			let [e, d, c, b, a] = ("" + v.n).split("").reverse();
-			let de = (d || "0") + e;
-			let opcode = +de;
-			v.pmindex = -1;
-			v.parameterMode = [+c || 0, +b || 0, +a || 0];
-			console.log("== opcode ", opcode);
-			await (intcodes["" + opcode] || intcodes["404"])();
+	let opcodes = {
+		"1": async () => {
+			let [a, b, out] = [v(), v(), p()];
+			memory[out] = a + b;
+		},
+		"2": async () => {
+			let [a, b, out] = [v(), v(), p()];
+			memory[out] = a * b;
+		},
+		"3": async () => {
+			let [out] = [p()];
+			let value = await getInput();
+			memory[out] = value;
+		},
+		"4": async () => {
+			let [value] = [v()];
+			writeOutput(value);
+		},
+		"5": async () => {
+			let [test, iftrue] = [v(), v()];
+			if (test !== 0n) {
+				instructionPointer = iftrue;
+				parameterIndex = 0n;
+			}
+		},
+		"6": async () => {
+			let [test, iffalse] = [v(), v()];
+			if (test === 0n) {
+				instructionPointer = iffalse;
+				parameterIndex = 0n;
+			}
+		},
+		"7": () => {
+			let [f, s, out] = [v(), v(), p()];
+			if (f < s) memory[out] = 1n;
+			else memory[out] = 0n;
+		},
+		"8": async () => {
+			let [f, s, out] = [v(), v(), p()];
+			if (f === s) memory[out] = 1n;
+			else memory[out] = 0n;
+		},
+		"9": async () => {
+			let [incrRelativeBy] = [v()];
+			relativeBase += incrRelativeBy;
+		},
+		"99": async () => {
+			exit = true;
+		},
+	};
+
+	while (!exit) {
+		parameterIndex = 0n;
+		parameterModes = [1];
+		let opcode = v();
+		let [e, d, c, b, a] = ("" + opcode).split("").reverse();
+		let de = (d || "0") + e;
+		parameterModes = [0, +c || 0, +b || 0, +a || 0];
+
+		if (opcodes["" + +de]) {
+			await opcodes["" + +de]();
+		} else {
+			throw new Error("opcode " + "" + +de + " does not exist.");
 		}
 
-		console.log(v.m.join(","));
+		instructionPointer += parameterIndex;
+	}
 
-		over.v = true;
-		await donestream.emit("done!");
-	}, 0);
-
-	return { ins: instream, outs: outstream, done: donestream, over };
+	console.log("Done!", memory.join(",").replace(/(\,0)+$/, ""));
 }
 
-let phaseSetting = [4, 3, 2, 1, 0];
-
-let inqrew = input;
 (async () => {
-	try {
-		let comp = runComputer(input);
-		await comp.ins.emit(1n);
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-		console.log("v", await comp.outs.next());
-	} catch (e) {
-		console.log(e);
-	}
+	intcode(
+		input,
+		async () => 1n,
+		v => console.log(v),
+	);
 })();
-
-// doarr(items).map(arrq => {
-// 	let val = doq(arrq);
-// 	rq.push(["new min", val, arrq]);
-// 	if (val > min) {
-// 		min = val;
-// 	}
-// });
-// console.log(rq.join("\n"));
-// console.log(min);
-
-// console.log(doq([4, 3, 2, 1, 0]));
--- solutions/day9/day9.2.js
+++ solutions/day9/day9.2.js
@@ -1,6 +1,117 @@
-/*
-declare let input: string;
-declare let output: any;
-copy(text: string): text
-print(text: string): undefined
-*/
+async function intcode(
+	input,
+	getInput /*: () => Promise<bigint>*/,
+	writeOutput /*: (v: bigint) => void */,
+) {
+	let memory = input.split(",").map(w => BigInt(+w));
+	for (let i = 0; i < 10000; i++) {
+		memory.push(0n);
+	}
+	let instructionPointer = 0n;
+
+	let relativeBase = 0n;
+
+	let parameterModes = [];
+	let parameterIndex = 0n;
+
+	let exit = false;
+
+	let getPointer_ = () => {
+		let parameterMode = parameterModes[parameterIndex];
+		if (parameterMode === 0) {
+			return memory[instructionPointer + parameterIndex];
+		}
+		if (parameterMode === 1) {
+			return instructionPointer + parameterIndex;
+		}
+		if (parameterMode === 2) {
+			return relativeBase + memory[instructionPointer + parameterIndex];
+		}
+	};
+	let getPointer = () => {
+		let res = getPointer_();
+		parameterIndex++;
+		return res;
+	};
+	let getValue = () => memory[getPointer()];
+	let p = getPointer;
+	let v = getValue;
+
+	let opcodes = {
+		"1": async () => {
+			let [a, b, out] = [v(), v(), p()];
+			memory[out] = a + b;
+		},
+		"2": async () => {
+			let [a, b, out] = [v(), v(), p()];
+			memory[out] = a * b;
+		},
+		"3": async () => {
+			let [out] = [p()];
+			let value = await getInput();
+			memory[out] = value;
+		},
+		"4": async () => {
+			let [value] = [v()];
+			writeOutput(value);
+		},
+		"5": async () => {
+			let [test, iftrue] = [v(), v()];
+			if (test !== 0n) {
+				instructionPointer = iftrue;
+				parameterIndex = 0n;
+			}
+		},
+		"6": async () => {
+			let [test, iffalse] = [v(), v()];
+			if (test === 0n) {
+				instructionPointer = iffalse;
+				parameterIndex = 0n;
+			}
+		},
+		"7": () => {
+			let [f, s, out] = [v(), v(), p()];
+			if (f < s) memory[out] = 1n;
+			else memory[out] = 0n;
+		},
+		"8": async () => {
+			let [f, s, out] = [v(), v(), p()];
+			if (f === s) memory[out] = 1n;
+			else memory[out] = 0n;
+		},
+		"9": async () => {
+			let [incrRelativeBy] = [v()];
+			relativeBase += incrRelativeBy;
+		},
+		"99": async () => {
+			exit = true;
+		},
+	};
+
+	while (!exit) {
+		parameterIndex = 0n;
+		parameterModes = [1];
+		let opcode = v();
+		let [e, d, c, b, a] = ("" + opcode).split("").reverse();
+		let de = (d || "0") + e;
+		parameterModes = [0, +c || 0, +b || 0, +a || 0];
+
+		if (opcodes["" + +de]) {
+			await opcodes["" + +de]();
+		} else {
+			throw new Error("opcode " + "" + +de + " does not exist.");
+		}
+
+		instructionPointer += parameterIndex;
+	}
+
+	console.log("Done!", memory.join(",").replace(/(\,0)+$/, ""));
+}
+
+(async () => {
+	intcode(
+		input,
+		async () => 2n,
+		v => console.log(v),
+	);
+})();
