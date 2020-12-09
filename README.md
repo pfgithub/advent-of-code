1e4d18599ae2bee5303bcc58362f57bbe120a100
pfg <pfg@pfg.pw>
2020-12-09 00:52:45 -0800

    auto input downloader (I hope it works)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 my adventofcode solutions and helper tool
 
 `yarn install`  
-`yarn go 2020 dayX` - get set up for a challenge  
+`yarn go 2020 X` - get set up for a challenge  
 `yarn watch` - start running code on save  
 `yarn prettierwatch` - format solution code on save
 
@@ -14,9 +14,9 @@ open 3-4 terminal windows
 - leave one blank
 - run `yarn watch` on one
 - run `yarn watchPrettier` on one (unless your editor is set to format code with prettier)
-- use the blank one to run `yarn go dayX` before the advent opens
+- use the blank one to run `yarn go 2020 X` before the advent opens
 - edit the code in a text editor
 - on save, your code will run and results will be shown
 - accidental infinite loops will be killed by re-saving the code
 - code may run twice because of prettier
-- paste your input into `dayX.txt` when you are ready to use it and get it from the `input` variable (as a string)
+- your input will be loaded into `dayX.txt`, read it from the `input` variable (as a string, trimmed automatically) or from `lines`, pre-split by `\n`, or from dblines, pre-split by `\n\n` and then by `\n`
--- setup.js
+++ setup.js
@@ -1,16 +1,23 @@
 const fs = require("fs");
 const path = require("path");
+// eslint-disable-next-line
+const fetch = require("node-fetch");
 
-let solnName = process.argv[3];
+let soln_day = process.argv[3];
 
 const year = process.argv[2];
 
-if(!year) throw new Error("!year");
-if(!solnName) throw new Error("!solnName");
+if(!year || !+year) throw new Error("!year");
+if(!soln_day || !+soln_day) throw new Error("!solnName");
+
+const solnName = "day"+soln_day;
 
 let initialText = fs.readFileSync(
 	path.join(__dirname, year, "/solutions", "_defaults", "_defaults.0.ts"),
 );
+
+const txtfile = solnName + ".txt";
+const txtfile_path = path.join(__dirname, year, "solutions", solnName, txtfile);
 
 fs.mkdirSync(path.join(__dirname, year, "solutions", solnName));
 [
@@ -19,13 +26,39 @@ fs.mkdirSync(path.join(__dirname, year, "solutions", solnName));
 	solnName + ".3.ts",
 	solnName + ".4.ts",
 	solnName + ".5.ts",
-	solnName + ".txt",
 ].forEach((v, i) =>
 	fs.writeFileSync(
 		path.join(__dirname, year, "solutions", solnName, v),
-		v.endsWith(".ts") ? initialText : "",
+		initialText,
 		"utf-8",
 	),
 );
+fs.writeFileSync(txtfile_path, "", "utf-8");
 
-console.log("done");
+console.log("done. waiting for the time…");
+
+const cookietext = fs.readFileSync(path.join(__dirname, "cookie.txt"));
+
+const res_time = new Date();
+res_time.setUTCSeconds(1); // buffer time
+res_time.setUTCMinutes(0);
+res_time.setUTCHours(5);
+res_time.setUTCDate(+soln_day);
+res_time.setUTCMonth(11);
+res_time.setUTCFullYear(+year);
+console.log(res_time.toString());
+
+const interval = setInterval(() => {
+	const wait_time = res_time.getTime() - Date.now();
+	if(wait_time < 0) return clearInterval(interval);
+	process.stdout.write("\r"+wait_time+"ms\x1b[K");
+}, 1000);
+
+setTimeout(async () => {
+	console.log("\nWait over! Fetching…");
+	const input_text = await (await fetch("https://adventofcode.com/"+year+"/day/"+soln_day+"/input", {
+		headers: {"Cookie": cookietext},
+	})).text();
+	fs.writeFileSync(txtfile_path, input_text, "utf-8");
+	console.log("Fetched!");
+}, res_time.getTime() - Date.now());
