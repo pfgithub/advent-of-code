a0793554fead0f6b25fa8b9f3017b024efdf8080
pfg <pfg@pfg.pw>
2020-12-10 21:29:27 -0800

    arrays for future use I guess

--- 2020/solutions/_defaults/_defaults.0.ts
+++ 2020/solutions/_defaults/_defaults.0.ts
@@ -4,4 +4,10 @@ copy(text: string) → clipboard
 error(message: string) → thrown error
 */
 
+// Cardinals:
+// [[1,0],[-1,0],[0,1],[0,-1]]
+// +Diagonals:
+// [[1,0],[-1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,-1],[1,1]]
+
 export {};
+
