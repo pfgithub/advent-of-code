518a638d11be1b28680f93199c93092151d8cfd9
pfg <pfg@pfg.pw>
2021-12-01 02:05:17 -0800

    uum uuh i forgot this was starting. anyway nice to get my tools set up and stuff. I need like a way to easily switch between practice input and real input and probably some stdlib stuff idk. anyway
    >   1   04:58:45  31867      0   05:02:40  26961      0
    
    so 4mins to get from part 1 to part 2 isn't great on this one. that was divided up between
    - trying to understand the part 2 thing (I had i - 1, i - 2 at first and that was clearly incorrect but it took a while to get)
    - messing around with const and let and idk
    - tryŋa disable the practice input

---
+++ 2021/solutions/day1/day1.1.ts
@@ -0,0 +1,35 @@
+/*
+input: string, lines: string[], dblines: string[][]
+copy(text: string) → clipboard
+error(message: string) → thrown error
+-5..mod(3) → @mod(-5, 3)
+*/
+
+// Cardinals:
+// [[1,0],[-1,0],[0,1],[0,-1]]
+// +Diagonals:
+// [[1,0],[-1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,-1],[1,1]]
+
+export {};
+
+const nv = (i: number, a: number[]) => {
+    return a[i] + a[i + 1] + a[i + 2];
+}
+
+let input_a;
+input_a = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
+
+const res = input.trim().split("\n").map(w =>+ w).map((m, i, a) => nv(i, a));
+console.log(res);
+const rv = res.map((w, i, a) => w > a[i - 1]);
+rv.shift();
+console.log(rv.reduce((t, a) => t + +a, 0));
