fb14e75d76df460c91b27a35b25b8a6336818ac6
pfg <pfg@pfg.pw>
2021-12-01 21:51:06 -0800

    more vec

--- 2021/solutions/day2/day2.4.ts
+++ 2021/solutions/day2/day2.4.ts
@@ -12,6 +12,25 @@ error(message: string) → thrown error
 
 export {};
 
-const practice = ``;
+const practice = `forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2`;
+
+const inv = input;
+
+let pos = vec(2, [0, 0]);
+let aim = 0;
+
+inv.trim().split("\n").map(w => w.split(" ")).forEach(([dir, cntv]) => {
+    const cnt = +cntv;
+    if(dir === "forward") pos = pos.add(vec(2, [cnt, aim * cnt]));
+    if(dir === "up") aim -= cnt;
+    if(dir === "down") aim += cnt;
+});
+
+(pos.x * pos.y).dwth(log)
 
 // input = practice;
