08fa410ca306832a1b4b1aa6763c9da3f9d48318
pfg <pfg@pfg.pw>
2021-12-13 00:20:53 -0500

    not super great. understood the problem fully but attempted at first with a method I knew would be less debuggable but hoped would work anyway. it didn't, so I had to rewrite it in the way that I knew would be needed for part 2 and was actually visually debuggable. - probably should have gone with the visual way from the start. I knew trying to do it with a Set() I would run into problems and I would have to make changes for part 2. so I should have just done the board from the start. - also I have to fix my downloader. wasn't expecting node-fetch to decide it couldn't be imported with require() because it didn't feel like it. how have web things adapted so easily to `import` but node stuff is just completely broken and not compatible

---
+++ 2021/solutions/day13/day13.1.ts
@@ -0,0 +1,190 @@
+import {fhmain} from "../../../src/fheader";
+fhmain(__filename);
+/*
+input: string, lines: string[], dblines: string[][]
+copy(text: string) → clipboard
+error(message: string) → thrown error
+-5..mod(3) → @mod(-5, 3)
+*/
+
+// Cardinals:
+// [[1,0],[-1,0],[0,1],[0,-1]]
+// +Diagonals:
+// [[1,0],[-1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,-1],[1,1]]
+
+export {};
+
+const practice = `
+6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5
+`;
+// input = practice;
+input = input.trim();
+
+
+
+
+const [dots, folds] = input.split("\n\n");
+
+const dts = dots.split("\n").map(v => v.split(",").map(Number));
+
+let board = makeBoard(".");
+
+for(const dot of dts) {
+    board.set(dot, "#");
+}
+
+
+const firstfold = folds.split("\n")[0].split(" ")[2].split("=");
+
+const res = new Set();
+
+const m = firstfold[0] === "x" ? 0 : 1;
+const n = 1 - m;
+
+function applyfold(fold) {
+    const newboard = makeBoard(".");
+
+    const m = firstfold[0] === "x" ? 0 : 1;
+    const q = m === 0 ? [+firstfold[1], 0] : [0, +firstfold[1]];
+    console.log(q);
+
+    board.forEach((tile, pos) => {
+        if(tile === "#") {
+            if(pos[m] > +fold[1]) {
+                newboard.set(
+                    pos.sub(q)
+                    .dwth(v => v[m] = -v[m])
+                    .add(q),
+                    "#",
+                );
+            }else{
+                newboard.set(pos, "#");
+            }
+        }
+    });
+    
+    board = newboard;
+}
+
+board.print().dwth(log);
+applyfold(firstfold);
+board.print().dwth(log);
+
+let count = 0;
+board.forEach((itm) => {
+    if(itm === "#") {
+        count++;
+    }
+});
+count.dwth(log); // expect=602
+
+
+
+
+
+
+type nobi = number | bigint;
+type Board<T> = {
+	get(pos: Point2D): T;
+	set(pos: Point2D, t: T): void;
+	clear(): void;
+	forEach(visitor: (v: T, pos: Point2D) => void): void;
+	print(printer?: (v: T, pos: Point2D) => string | nobi): string;
+	copy(): Board<T>;
+};
+function makeBoard<T>(fill: T): Board<T> {
+	// it would be useful if board could center at 0,0 and expand infinitely
+	let board: T[][] = [];
+	let limits:
+		| { min: Point2D, max: Point2D }
+		| undefined;
+	let reso: Board<T> = {
+		clear: () => {
+			board = [];
+		},
+		get: (pos) => {
+			if (!limits) return fill;
+			if (
+                pos.op(limits.min, (a, b) => a < b).some(w => w) ||
+                pos.op(limits.max, (a, b) => a > b).some(w => w)
+            ) return fill;
+			if (!board[pos.y]) return fill;
+			let bval = board[pos.y][pos.x];
+			return bval === undefined ? fill : bval;
+		},
+		set: (pos, v) => {
+			if (!limits) {
+				limits = {min: dupe(pos), max: dupe(pos)};
+            }
+            limits.min = pos.op(limits.min, (a, b) => Math.min(a, b));
+            limits.max = pos.op(limits.max, (a, b) => Math.max(a, b));
+			if (!board[pos.y]) board[pos.y] = [];
+			board[pos.y][pos.x] = v;
+		},
+		forEach: visitor => {
+			if (!limits) return;
+			for (let y = limits.min.y; y <= limits.max.y; y++) {
+				for (let x = limits.min.x; x <= limits.max.x; x++) {
+					visitor(reso.get([x, y]), [x, y]);
+				}
+			}
+		},
+		copy: () => {
+			let nb = makeBoard<T>(fill);
+			reso.forEach((v, pos) => nb.set(pos, v));
+			return nb;
+		},
+		print: (printer = v => v as any): string => {
+			// ratelimit print
+			if (!limits) return "*no board to print*";
+			let ylength = 0;
+			for (let y = limits.min.y - 1; y <= limits.max.y + 1; y++) {
+				ylength = Math.max(y.toString().length, ylength);
+			}
+			const resc: string[] = [];
+			let llen: number = limits.max.x - limits.min.x + 3;
+			for (let y = limits.min.y - 1; y <= limits.max.y + 1; y++) {
+				let line = "";
+				for (let x = limits.min.x - 1; x <= limits.max.x + 1; x++) {
+					line += printer(reso.get([x, y]), [x, y]);
+				}
+				if(line.length > llen) llen = line.length;
+				resc.push(y.toString().padStart(ylength, " ") + " | " + line + " |");
+			}
+			resc.unshift(
+				" ".repeat(ylength) +
+					" .-" +
+					"-".repeat(llen) +
+					"-. " + (limits.min.y - 1) + " .. " + (limits.max.y + 1),
+			);
+			resc.push(
+				" ".repeat(ylength) +
+					" '-" +
+					"-".repeat(llen) +
+					"-'",
+			);
+			return resc.join("\n");
+		},
+	};
+	return reso;
+}
---
+++ 2021/solutions/day13/day13.3.ts
@@ -0,0 +1,25 @@
+import {fhmain} from "../../../src/fheader";
+fhmain(__filename);
+/*
+input: string, lines: string[], dblines: string[][]
+copy(text: string) → clipboard
+error(message: string) → thrown error
+-5..mod(3) → @mod(-5, 3)
+*/
+
+// Cardinals:
+// [[1,0],[-1,0],[0,1],[0,-1]]
+// +Diagonals:
+// [[1,0],[-1,0],[0,1],[0,-1],[-1,-1],[-1,1],[1,-1],[1,1]]
+
+export {};
+
+const practice = ``;
+// input = practice;
+input = input.trim();
+
+// input.
+
+// hi! i'm glad you're excited to code
+// but consider fully reading the problem statement first.
+// Sincerely, future you.
